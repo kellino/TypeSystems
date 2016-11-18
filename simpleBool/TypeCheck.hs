85680739069703d40e5e1a63acaf5caf6882db43
David Kelly <dkellino@gmail.com>
2016-11-18 19:38:10 +0000

    initial commit

---
+++ simpleBool/TypeCheck.hs
@@ -0,0 +1,35 @@
+module TypeCheck where
+
+import Syntax
+import Control.Monad.Except
+import Control.Monad.Identity
+
+type Type a = ExceptT String Identity a
+
+typeof :: Term -> Type Ty
+typeof (Var _) = return TyBool -- placeholder
+typeof TmTrue = return TyBool
+typeof TmFalse = return TyBool
+typeof Abs{} = return TyBool -- placeholder
+typeof (App t1 t2) = do
+    t1' <- typeof t1
+    t2' <- typeof t2
+    case t1' of
+         TyArr t11 t12 ->
+             if t11 == t2' 
+                then return t12
+                else throwError "parameter type mismatch"
+         _ -> throwError "expected arrow type"
+typeof (If b t1 t2) = do
+    b' <- typeof b
+    case b' of
+         TyBool -> do
+             t1' <- typeof t1
+             t2' <- typeof t2
+             if t1' /= t2'
+                then throwError "arms of conditional have different types"
+                else return t1'
+         _  -> throwError "guard of conditional is not a boolean"
+
+
+runTypeOf = runIdentity . runExceptT
