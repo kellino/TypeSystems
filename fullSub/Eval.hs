bcc071b660a956bb745027c069e0173e22d66c5f
David Kelly <dkellino@gmail.com>
2016-12-07 09:49:07 +0000

    added more terms

--- fullSub/Eval.hs
+++ fullSub/Eval.hs
@@ -13,17 +13,38 @@ runEval :: Term -> Either String Term
 runEval = runFreshM . runExceptT . eval
 
 eval :: Term -> Eval Term
+eval TmError = throwError "Exception encountered"
+eval str@TmString{} = return str
 eval v@TmVar{} = return v
+eval l@TmAbs{} = return l
+eval a@TmAscription{} = return a
 eval TmTrue = return TmTrue
 eval TmFalse = return TmFalse
 eval TmZero = return TmZero
 eval (TmIsZero TmZero) = return TmTrue
 eval (TmIsZero _) = return TmFalse
 eval f@TmFloat{} = return f
+eval s@TmSucc{} =  return s
+eval p@TmPred{} = return p
 eval (TmFloatTimes (TmFloat x) (TmFloat y)) = return $ TmFloat (x * y)
 eval (TmLet bnd) = do
     ((x, unembed -> t1), t2) <- unbind bnd
     t1' <- eval t1
     let body = subst x t1' t2
     eval body
+eval (TmFix e) = eval (TmApp e (TmFix e))
+eval (TmIf b t1 t2) = do
+    b' <- eval b
+    case b' of
+         TmTrue -> eval t1
+         TmFalse -> eval t2
+         x -> throwError $ show x 
+eval (TmApp e1 e2) = do
+    v2 <- eval e2
+    case e1 of
+        (TmAbs bnd) -> do
+            ((x, _), body) <- unbind bnd
+            let body' = subst x v2 body
+            eval body'
+        _ -> throwError "first expression is not a lambda abstraction"
 eval x = throwError $ "not yet implemented: " ++ show x
