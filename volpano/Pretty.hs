2c2fc638f0d44019b1f279336b52106547bd5c5d
David Kelly <dkellino@gmail.com>
2017-02-25 11:41:37 +0000

    pointless fiddling with colours

--- volpano/Pretty.hs
+++ volpano/Pretty.hs
@@ -19,18 +19,21 @@ instance Display String where
     display = text
 
 instance Display Expr where
-    display (Skip l) = text "skip : " <> display l <> " Cmd"
+    display (Skip l) = bold $ text "skip : " <> display TyUnit <> display l <> cmd
     display (BoolExpr TmTrue l) = text "true : " <> display TyBool <> display l
     display (BoolExpr TmFalse l) = text "false : " <> display TyBool <> display l
     display (Num n l) = display (show n) <> text " : " <> display TyNum <> display l
-    display (Assign (Var x l) (Num n _)) = "var "  <> text x <> text " = " <> text (show n) <> text " : " <> display l <> text " Cmd"
+    display (Assign (Var x l) (Num n _)) = "var "  <> text x <> text " = " <> text (show n) <> text " : " <> display l <> cmd
     display (Var x l) = text "var " <> display x <> text " : " <> display l
     display x = text $ show x
 
+cmd :: Doc
+cmd = bold . red $ text " Cmd"
+
 instance Display Label where
-    display Low = text "Low"
-    display Medium = text "Medium"
-    display High = text "High"
+    display Low = ondullgreen $ text "Low"
+    display Medium = ondullblue $ text "Medium"
+    display High = ondullcyan $ text "High"
 
 instance Display Ty where
     display TyBool = green $ text "Bool "
