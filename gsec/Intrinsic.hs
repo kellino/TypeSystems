c0bf34555d75b4d65f711d99bec7856e68183e1d
David Kelly <dkellino@gmail.com>
2017-03-19 11:32:13 +0000

    wip

--- gsec/Intrinsic.hs
+++ gsec/Intrinsic.hs
@@ -6,10 +6,36 @@ import Lattice
 import Control.Monad.Except
 import Unbound.Generics.LocallyNameless
 
-type Eval a = ExceptT Doc FreshM a
+type Eval a = ExceptT String FreshM a
 
-runEval :: Term -> Either Doc Term
+runEval :: Term -> Either String Term
 runEval = runFreshM . runExceptT . eval
 
 eval :: Term -> Eval Term
-eval = undefined
+eval v@Var{} = return v
+eval v@Val{} = return v
+eval (Op op b1 b2) = do
+    b1' <- eval b1
+    b2' <- eval b2
+    case op of
+         And -> b1' `bAnd` b2'
+         Or -> b1' `bOr` b2'
+         Implies -> b1' `bImplies` b2'
+eval _ = throwError "undefined"
+
+
+-- helper functions for boolean operators
+bAnd :: Term -> Term -> Eval Term
+bAnd (Val TmTrue l) (Val TmTrue l') = return (Val TmTrue (l \/ l'))
+bAnd (Val _ l) (Val _ l') = return (Val TmFalse (l \/ l'))
+bAnd _ _ = throwError "undefined"
+
+bOr :: Term -> Term -> Eval Term
+bOr (Val TmTrue l) (Val TmTrue l') = return (Val TmTrue (l \/ l'))
+bOr (Val _ l) (Val _ l') = return (Val TmFalse (l \/ l'))
+bOr _ _ = throwError "undefined"
+
+bImplies :: Term -> Term -> Eval Term
+bImplies (Val TmTrue l) (Val TmTrue l') = return (Val TmTrue (l \/ l'))
+bImplies (Val _ l) (Val _ l') = return (Val TmFalse (l \/ l'))
+bImplies _ _ = throwError "undefined"
